75621927b3fdb3a7612c38e577778479aafe695d
Denise Cho <86213644+YHyunCho@users.noreply.github.com>
2024-06-07 18:59:46 +0900

    Challenge2_Play Fetch

---
+++ Prototype 2/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    private float sendCooldown = 3f;
+    private bool canPress = true;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space) && canPress)
+        {
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            canPress = false;
+
+            Invoke("ResetPress", sendCooldown);
+        }
+    }
+
+    void ResetPress()
+    {
+        canPress = true;
+    }
+}
